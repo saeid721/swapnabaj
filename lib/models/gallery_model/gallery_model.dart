ba6b707adba9e43232fde7c428b071664be4f798
saeid721 <saeid721km@gmail.com>
2024-10-14 17:28:15 +0600

    update

--- lib/models/gallery_model/gallery_model.dart
+++ lib/models/gallery_model/gallery_model.dart
@@ -1,17 +1,17 @@
 class GalleryModel {
-  final String title;
+  final String imageTitle;
   final String description;
   final String? fileUrl;
 
   GalleryModel({
-    required this.title,
+    required this.imageTitle,
     required this.description,
     this.fileUrl,
   });
 
   factory GalleryModel.fromDocument(Map<String, dynamic> data, String id) {
     return GalleryModel(
-      title: data['title'] ?? '',
+      imageTitle: data['title'] ?? '',
       description: data['description'] ?? '',
       fileUrl: data['file_url'], // Fetching image URL from Firestore
     );
--- lib/views/dashboard/admin_gallery_screen/admin_gallery_screen.dart
+++ lib/views/dashboard/admin_gallery_screen/admin_gallery_screen.dart
@@ -10,26 +10,26 @@ import '../../../global_widget/input_decoration.dart';
 import '../admin_login_screen/admin_login_screen.dart';
 import 'admin_gallery_details_screen.dart';
 
-List<ImageDetails> images = [
-  ImageDetails(
+List<GalleryModel> images = [
+  GalleryModel(
     fileUrl: 'https://t3.ftcdn.net/jpg/02/09/43/80/360_F_209438048_bqYPR1SZJx583icNF2fasiwfnttqMZZn.jpg',
     imageTitle: 'Swapnobaj',
     description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   ),
-  ImageDetails(
+  GalleryModel(
     fileUrl: 'https://t3.ftcdn.net/jpg/04/97/45/58/360_F_497455841_1TSJ07nyEcSOIzYJ4nevIGtEe0VOPWTF.jpg',
     imageTitle: 'Swapnobaj',
     description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   ),
-  ImageDetails(
+  GalleryModel(
     fileUrl: 'https://t3.ftcdn.net/jpg/02/09/43/80/360_F_209438048_bqYPR1SZJx583icNF2fasiwfnttqMZZn.jpg',
     imageTitle: 'Swapnobaj',
     description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   ),
-  ImageDetails(
+  GalleryModel(
     fileUrl: 'https://t3.ftcdn.net/jpg/04/97/45/58/360_F_497455841_1TSJ07nyEcSOIzYJ4nevIGtEe0VOPWTF.jpg',
     imageTitle: 'Swapnobaj',
     description:
@@ -212,12 +212,12 @@ class _AdminGalleryScreenState extends State<AdminGalleryScreen> {
   }
 }
 
-class ImageDetails {
+class GalleryModel {
   final String fileUrl;
   final String imageTitle;
   final String description;
 
-  ImageDetails({
+  GalleryModel({
     required this.fileUrl,
     required this.imageTitle,
     required this.description,
